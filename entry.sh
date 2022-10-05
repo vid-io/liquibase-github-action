92a420091f1cdbbb1987fc22297364fc81c0fff4
Vitalii Petrenko <vitalii.petrenko@vid.io>
2022-10-05 16:59:01 +0300

    run all comm

--- entry.sh
+++ entry.sh
@@ -217,4 +217,5 @@ check_optional_param "$OPERATION" hubApiKey $HUBAPIKEY
 check_optional_param "$OPERATION" licenseKey $LICENSEKEY
 validate_operation
 
-docker-entrypoint.sh "${PARAMS[@]}" $OPERATION "${VALUES[@]}" "${SECONDPARAMS[@]}"
+# docker-entrypoint.sh "${PARAMS[@]}" $OPERATION "${VALUES[@]}" "${SECONDPARAMS[@]}"
+docker-entrypoint.sh "$*"
